0368f4c1a30dc2d6517930fa2067e8bfda41b522
acollens <acollens@umich.edu>
2023-07-25 16:06:49 -0400

    Running workflow with smaller datasets

--- workflow/scripts/submit_slurm.sh
+++ workflow/scripts/submit_slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=mikropml # sbatch options here only affect the overall job
+#SBATCH --job-name=mikropml_0725
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=100MB
-#SBATCH --time=96:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=log/hpc/slurm-%j_%x.out 
-#SBATCH --account=YOUR_ACCOUNT_HERE      # your account name
-#SBATCH --partition=standard             # the partition
-#SBATCH --mail-user=YOUR_EMAIL_HERE      # your email
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard             
+#SBATCH --mail-user=acollens@umich.edu      
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 # Load any required modules for your HPC.
@@ -19,4 +19,4 @@
 module load singularity 
 
 # Run snakemake
-snakemake --profile config/slurm --latency-wait 90 --use-singularity --use-conda --conda-frontend mamba --configfile config/test.yml 
+snakemake --profile config/slurm --latency-wait 90 --use-singularity --use-conda --conda-frontend mamba --configfile config/glmnet.yml 
