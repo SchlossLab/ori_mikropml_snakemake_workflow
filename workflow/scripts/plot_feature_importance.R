ebaa75f012d62967d1f3f24bfc6a52d6c929d34f
acollens <acollens@umich.edu>
2023-07-27 15:47:24 -0400

    changed "feat" back to "names" to avoid error

--- workflow/scripts/plot_feature_importance.R
+++ workflow/scripts/plot_feature_importance.R
@@ -10,13 +10,13 @@ top_n <- as.numeric(snakemake@params[["top_n"]])
 #top_n <- as.numeric(20)
 
 top_feats <- feat_df %>%
-  group_by(method, feat) %>%
+  group_by(method, names) %>%
   summarize(median_diff = median(perf_metric_diff)) %>%
   slice_max(order_by = median_diff, n = top_n)
 
 feat_plot <- feat_df %>%
-    right_join(top_feats, by = c("method", "feat")) %>%
-    mutate(features = factor(feat, levels = unique(top_feats$feat))) %>%
+    right_join(top_feats, by = c("method", "names")) %>%
+    mutate(features = factor(names, levels = unique(top_feats$names))) %>%
     ggplot(aes(x = perf_metric_diff, y = features, color = method)) +
     geom_boxplot() +
     facet_wrap(~method) +
